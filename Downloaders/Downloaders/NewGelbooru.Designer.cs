6808087ec3595934a399f5688ab068d6830140ed
Elbanablade <ryan.reasoner@gmail.com>
2014-03-02 23:45:16 -0700

    gelbooru add interface re-organized

--- Downloaders/Downloaders/NewGelbooru.Designer.cs
+++ Downloaders/Downloaders/NewGelbooru.Designer.cs
@@ -61,7 +61,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 524);
+            this.button1.Location = new System.Drawing.Point(12, 280);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 20;
@@ -71,7 +71,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(93, 524);
+            this.button2.Location = new System.Drawing.Point(93, 280);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 30;
@@ -339,7 +339,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 559);
+            this.ClientSize = new System.Drawing.Size(340, 322);
             this.Controls.Add(this.cbGLEScore);
             this.Controls.Add(this.cbGLEHeight);
             this.Controls.Add(this.cbGLEWidth);
